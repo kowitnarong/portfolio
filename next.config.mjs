459ba002761feacba2c7725f947cbe3417883f77
kowit-desktop <kowit.narongtuwapan@hotmail.com>
2024-05-31 14:46:41 +0700

    feat: Remove unnecessary code in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -26,11 +26,6 @@ const nextConfig = {
         permanent: true,
         destination: process.env.NEXT_PUBLIC_WWW_SITE_URL + '/:path*',
       },
-      {
-        source: '/',
-        destination: '',
-        permanent: true,
-      },
     ];
   },
   reactStrictMode: process.env.NODE_ENV !== 'development',
